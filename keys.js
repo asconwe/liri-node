5f1779445cf1257eec963782735818a262ff4bdf
August Conwell <august.conwell@gmail.com>
2017-04-07 06:52:10 -0400

    Initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'VgyJlDlsE4DaEWSOESFCmtee6',
+  consumer_secret: 'GaUU7OCvzSXoDJXGWX7OXRn68uNi0pw84McStaVH62sOSqrPEJ',
+  access_token_key: '2366053302-94vm7vEZD3mL7XZPJXBQgf2qfIlepnoneiN1GGk',
+  access_token_secret: 'wydBKZnZHrvel0sbWrNvcaGzYeVvGUdhzhG2bFMkvnYNl',
+}
---
+++ liri.js
@@ -0,0 +1,5 @@
+var fs = require('fs');
+var request = require('request');
+var keys = require('./keys.js');
+
+console.log(keys.consumer_key);
