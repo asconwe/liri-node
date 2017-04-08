a80f50be1afd0b431f50480f6aee8fd57c870cb4
August Conwell <august.conwell@gmail.com>
2017-04-07 21:42:41 -0400

    solved homework

--- keys.js
+++ keys.js
@@ -1,4 +1,3 @@
-console.log('this is loaded');
 
 exports.twitterKeys = {
   consumer_key: 'VgyJlDlsE4DaEWSOESFCmtee6',
--- liri.js
+++ liri.js
@@ -1,16 +1,22 @@
 var fs = require('fs');
 var request = require('request');
+var twitter = require('twitter');
+var spotify = require('spotify');
 var keys = require('./keys.js').twitterKeys;
 
 var action = process.argv[2];
-var arg = process.argv[3]
+var arg = process.argv[3];
+
 
 var getTweets = function getTweets() {
-  var qUrl = '' // Make this URL from twitter keys
-  request(qUrl, function(error, response, body) {
+  twitter(keys).get('statuses/user_timeline', 'count=20', function(error, tweets, response) {
     // console.log last 20 tweets and when they were created
     if (error) {
-      console.log('There was an issue getting your tweeets', error);
+      console.log('Couldn\'t access your tweets!');
+    } else {
+      tweets.forEach(function(tweet) {
+        console.log(tweet.created_at + ": " + tweet.text);
+      });
     }
   });
 }
@@ -19,10 +25,18 @@ var getSong = function getSong(song) {
   if (song === undefined) {
       song = "The Sign";
   }
-  var qUrl = '' // Make this URL from spotify api and song to search
-  request(qUrl, function(error, response, body) {
+  spotify.search({type: 'track', query: song }, function(error, response) {
     if (error) {
       console.log('There was an issue finding that song', error);
+    } else {
+      // console.log(response);
+      response.tracks.items.forEach(function(song) {
+        console.log('Song name:', song.name);
+        console.log('Artist:', song.album.artists[0].name);
+        console.log('Spotify preview URL:', song.preview_url);
+        console.log('Album:', song.album.name);
+        console.log('---');
+      });
     }
   });
 }
@@ -31,10 +45,20 @@ var getMovie = function getMovie(movie) {
   if (movie === undefined) {
     movie = "Mr. Nobody";
   }
-  var qUrl = '' // Make this URL from omdb api and movie to search
+  var qUrl = 'http://www.omdbapi.com/?t=' + movie;
   request(qUrl, function(error, response, body) {
     if (error) {
       console.log('There was an issue finding that movie', error);
+    } else {
+      flick = JSON.parse(body);
+      console.log('Title:', flick.Title);
+      console.log('Released:', flick.Year);
+      console.log('IMDB rating:', flick.Ratings[0].Value);
+      console.log('Country of origin:', flick.Country);
+      console.log('Language:', flick.Language);
+      console.log('Plot', flick.Plot);
+      console.log('Actors:', flick.Actors);
+      console.log('Rotten Tomatoes rating:', flick.Ratings[1].Value);
     }
   });
 }
@@ -42,11 +66,12 @@ var getMovie = function getMovie(movie) {
 var doThing = function doThing() {
   // get arguments from random.text
   var randAction, randArgument;
-  fs.readFile('random.txt', function(error, data) {
-    randAction = data[0];
-    randArgument = data[1];
+  fs.readFile('./random.txt', 'utf-8', function(error, data) {
+    var arr = data.split(',');
+    randAction = arr[0];
+    randArgument = arr[1];
+    actions[randAction](randArgument);
   });
-  actions[randAction](randArgument);
 }
 
 var actions = {
@@ -56,5 +81,5 @@ var actions = {
   'do-what-it-says': doThing 
 }
 
-actions[action](argument);
+actions[action](arg);
 
